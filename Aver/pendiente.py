1d8ce28c70e04ccc167f6d934056d06ec8b604a9
Aldair9819 <aldair.gr@culiacan.tecnm.mx>
2024-03-01 17:58:38 -0700

    Cambiso para ver trabajo Hectorin

---
+++ Aver/pendiente.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import os
+from PIL import Image as im
+import pickle as pk
+
+dfx = pd.read_pickle('x.x')
+dfy = pd.read_pickle('y.y')
+print(dfx[0])
+print(dfy[0])
